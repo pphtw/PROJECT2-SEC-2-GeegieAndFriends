722fb400df8c31c79d1015b4f9237ad6ce0dec52
pphtw <phutawan.palakavong@gmail.kmutt.ac.th>
2023-01-28 23:27:14 +0700

    edit style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,10 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      '112': '24rem',
+      height: {
+        '128': '28rem',
+      },
     },
-  },
   plugins: [],
+  }
 }
