6420e2d89ff0ed6f68c737a87b0c0fad76a5f3f8
Banlearit Siriboon <banlearit.ares@mail.kmutt.ac.th>
2023-01-29 18:46:54 +0700

    feat(player-controls): add new upload feature

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 export default{
   data() {
-      return{
+    return {
       audio: null,
       circleLeft: null,
       barWidth: null,
@@ -10,102 +10,174 @@ export default{
       currentTime: null,
       isTimerPlaying: false,
       repeat: true,
-      tracks:[
-          {
-            song : "Matsuri",
-            artist : "Fuji Kaze",
-            cover : "https://e.snmc.io/i/600/s/5126c31850a2de84d4d561c1c45cdeac/9808581/%E8%97%A4%E4%BA%95%E9%A2%A8-fujii-kaze-%E3%81%BE%E3%81%A4%E3%82%8A-matsuri-Cover-Art.jpg",
-            url : "../src/assets/tracks/matsuri.mp3",
-            favorited : false
-          },
-          {
-            song : "Double take",
-            artist : "dhruv",
-            cover : "https://i.scdn.co/image/ab67616d0000b273834f16100678d3e800fb5fb9",
-            url : "../src/assets/tracks/double_take.mp3",
-            favorited : false
-          },
-          {
-            song : "Recall",
-            artist : "bowkylion",
-            cover : "https://e.snmc.io/i/600/s/8551a37b85a88cff3cd699d7b0bd74f2/10164638/bowkylion-%E0%B8%A7%E0%B8%B2%E0%B8%94%E0%B9%84%E0%B8%A7%E0%B9%89-recall-Cover-Art.jpg",
-            url : "../src/assets/tracks/recall.mp3",
-            favorited : false
-          }
-        ],
+      tracks: [
+        {
+          name: "Matsuri",
+          artist: "Fuji Kaze",
+          cover: "https://e.snmc.io/i/600/s/5126c31850a2de84d4d561c1c45cdeac/9808581/%E8%97%A4%E4%BA%95%E9%A2%A8-fujii-kaze-%E3%81%BE%E3%81%A4%E3%82%8A-matsuri-Cover-Art.jpg",
+          source: "../src/assets/tracks/matsuri.mp3",
+          favorited: false
+        },
+        {
+          name: "Double take",
+          artist: "dhruv",
+          cover: "https://i.scdn.co/image/ab67616d0000b273834f16100678d3e800fb5fb9",
+          source: "../src/assets/tracks/double_take.mp3",
+          favorited: false
+        },
+        {
+          name: "Recall",
+          artist: "bowkylion",
+          cover: "https://e.snmc.io/i/600/s/8551a37b85a88cff3cd699d7b0bd74f2/10164638/bowkylion-%E0%B8%A7%E0%B8%B2%E0%B8%94%E0%B9%84%E0%B8%A7%E0%B9%89-recall-Cover-Art.jpg",
+          source: "../src/assets/tracks/recall.mp3",
+          favorited: false
+        }
+      ],
       currentTrack: null,
       currentTrackIndex: 0,
       transitionName: null
-      };
-    },
-  computed:{
-    currentTrackSong(){
-      if (this.currentTrackIndex !== null){
-        return this.tracks[this.currentTrackIndex].song
-      }
-    },
-    currentTrackArtist(){
-      if (this.currentTrackIndex !== null){
-        return this.tracks[this.currentTrackIndex].artist
-      }
     }
   },
   methods: {
-  favorite() {
-    // console.log(this.tracks[this.currentTrackIndex].favorited);
-    this.tracks[this.currentTrackIndex].favorited = !this.tracks[this.currentTrackIndex].favorited;
-    console.log(this.tracks[this.currentTrackIndex].favorited);
-  },
-  play(){
-    alert("yeah")
-    if(this.audio.paused){
+
+    play() {
+      if (this.audio.paused) {
+        this.audio.play();
+        this.isTimerPlaying = true;
+      } else {
+        this.audio.pause();
+        this.isTimerPlaying = false;
+      }
+    },
+    generateTime() {
+      let width = (100 / this.audio.duration) * this.audio.currentTime;
+      this.barWidth = width + "%";
+      this.circleLeft = width + "%";
+      let durmin = Math.floor(this.audio.duration / 60);
+      let dursec = Math.floor(this.audio.duration - durmin * 60);
+      let curmin = Math.floor(this.audio.currentTime / 60);
+      let cursec = Math.floor(this.audio.currentTime - curmin * 60);
+      if (durmin < 10) {
+        durmin = "0" + durmin;
+      }
+      if (dursec < 10) {
+        dursec = "0" + dursec;
+      }
+      if (curmin < 10) {
+        curmin = "0" + curmin;
+      }
+      if (cursec < 10) {
+        cursec = "0" + cursec;
+      }
+      this.duration = durmin + ":" + dursec;
+      this.currentTime = curmin + ":" + cursec;
+    },
+    updateBar(x) {
+      let progress = this.$refs.progress;
+      let maxduration = this.audio.duration;
+      let position = x - progress.offsetLeft;
+      let percentage = (100 * position) / progress.offsetWidth;
+      if (percentage > 100) {
+        percentage = 100;
+      }
+      if (percentage < 0) {
+        percentage = 0;
+      }
+      this.barWidth = percentage + "%";
+      this.circleLeft = percentage + "%";
+      this.audio.currentTime = (maxduration * percentage) / 100;
       this.audio.play();
-      this.isTimerPlaying = true
-    } else {
+    },
+    clickProgress(e) {
+      this.isTimerPlaying = true;
       this.audio.pause();
-      this.isTimerPlaying = false
-    }
+      this.updateBar(e.pageX);
+    },
+    prevTrack() {
+      this.transitionName = "scale-in";
+      this.isShowCover = false;
+      if (this.currentTrackIndex > 0) {
+        this.currentTrackIndex--;
+      } else {
+        this.currentTrackIndex = this.tracks.length - 1;
+      }
+      this.currentTrack = this.tracks[this.currentTrackIndex];
+      this.resetPlayer();
+    },
+    nextTrack() {
+      this.transitionName = "scale-out";
+      this.isShowCover = false;
+      if (this.currentTrackIndex < this.tracks.length - 1) {
+        this.currentTrackIndex++;
+      } else {
+        this.currentTrackIndex = 0;
+      }
+      this.currentTrack = this.tracks[this.currentTrackIndex];
+      this.resetPlayer();
+    },
+    stop(){
+      this.isTimerPlaying = false;
+    },
+    playLoop() {
+      if (this.repeat) {
+        this.currentTrackIndex = this.currentTrackIndex;
+      } else if (this.currentTrackIndex !== this.tracks.length - 1) {
+        this.currentTrackIndex++;
+      } else {
+        this.currentTrackIndex = 0;
+      }
+      this.currentTrack = this.tracks[this.currentTrackIndex];
+      this.resetPlayer();
+    },
+    playRandom() {
+      this.stop();
+      let randomIndex = Math.floor(Math.random() * this.tracks.length);
+      if (randomIndex === this.currentTrackIndex) {
+        randomIndex = this.playRandom();
 
+      } else {
+        this.currentTrackIndex = randomIndex;
+        this.currentTrack = this.tracks[this.currentTrackIndex];
+        this.resetPlayer();
+        this.play()
+      }
+      console.log(this.currentTrackIndex);
+    },
+    resetPlayer() {
+      this.barWidth = 0;
+      this.circleLeft = 0;
+      this.audio.currentTime = 0;
+      this.audio.src = this.currentTrack.source;
+      setTimeout(() => {
+        if(this.isTimerPlaying) {
+          this.audio.play();
+        } else {
+          this.audio.pause();
+        }
+      }, 300);
+    },
+    favorite() {
+      // console.log(this.tracks[this.currentTrackIndex].favorited);
+      this.tracks[this.currentTrackIndex].favorited = !this.tracks[this.currentTrackIndex].favorited;
+      console.log(this.tracks[this.currentTrackIndex].favorited);
+    },
   },
-  playTrack(){
-    this.isTimerPlaying = true
-  },
-  pauseTrack(){
-    this.isTimerPlaying = false
-  },
-  prevTrack(){
-    if (this.currentTrackIndex === 0){
-      this.currentTrackIndex = this.tracks.length - 1
-    }else {
-      this.currentTrackIndex--
-    }
-  },
-  nextTrack(){
-    if (this.currentTrackIndex === this.tracks.length -1){
-      this.currentTrackIndex = 0
-    }else {
-      this.currentTrackIndex++
-    }
-  },
-  playRandom(){
-    let randomIndex = Math.floor(Math.random() * this.tracks.length);
-    if (randomIndex === this.currentTrackIndex){
-      randomIndex = this.playRandom()
-    }else {
-      this.currentTrackIndex = randomIndex
-    }
-
-  },
-  playLoop(){
-    if (this.repeat){
-      this.currentTrackIndex = this.currentTrackIndex
-    }else if (this.currentTrackIndex !== this.tracks.length -1){
-      this.currentTrackIndex++
-    }
-    console.log(this.currentTrackIndex);
+  created() {
+    let vm = this;
+    this.currentTrack = this.tracks[0];
+    this.audio = new Audio();
+    this.audio.src = this.currentTrack.source;
+    this.audio.ontimeupdate = function() {
+      vm.generateTime();
+    };
+    this.audio.onloadedmetadata = function() {
+      vm.generateTime();
+    };
+    this.audio.onended = function() {
+      vm.nextTrack();
+      this.isTimerPlaying = true;
+    };
   }
-}
-
 }
 
 </script>
@@ -117,10 +189,11 @@ export default{
     <div id="card" class="w-72 h-128 rounded-md bg-gray-300 flex flex-col">
 
       <!-- Song Image -->
-      <div class="flex justify-center items-center w-full h-3/5 bg-cover rounded-t-md" :style="{'backgroundImage': `url(${tracks[currentTrackIndex].cover})`}">
-        <!-- <img id="song-cover" class="rounded-md"
+      <div class="flex justify-center items-center w-full h-3/5 bg-cover rounded-t-md"
+           :style="{'backgroundImage': `url(${tracks.cover})})`}">
+        <img id="song-cover" class="rounded-md"
         v-if="currentTrackIndex !== null"
-         :src="tracks[currentTrackIndex].cover"> -->
+         :src="tracks[currentTrackIndex].cover">
       </div>
 
       <!-- Song Details -->
@@ -128,9 +201,9 @@ export default{
         <div class="flex justify-around">
           <div class="w-10"></div>
 
-          <div class="text-center">
-            <h2 id="music-name" >{{currentTrackSong}}</h2>
-            <p id="music">{{currentTrackArtist}}</p>
+          <div class="text-center" v-if="currentTrack">
+            <h2 id="music-name" >{{currentTrack.artist}}</h2>
+            <p id="music">{{currentTrack.name}}</p>
           </div>
 
           <!-- Song favorited -->
@@ -155,19 +228,19 @@ export default{
 
       <!-- Track Time -->
         <div class="flex items-center pl-6 pr-6">
-          <div class="time-font-size flex-none">00:00</div>
+          <div class="time-font-size flex-none">{{ currentTime }}</div>
             <!-- <div class="flex-end w-full bg-gray-200 h-1 mx-1 rounded-full">
               <div class="bg-gray-600 h-1 rounded-full" style="width: 45%"></div>
             </div> -->
             <div class="w-4/6 flex-1 pl-1 pr-1"><input type="range" value="0" ref="progress" id="progress" ></div>
-          <div class="time-font-size flex-none">00:00</div>
+          <div class="time-font-size flex-none">{{ duration }}</div>
 
         </div>
 
       
         <div class="flex justify-around p-3 pb-0 items-center"> 
         <!-- random tag -->
-       <div class="random-track" @click="playRandom ">
+       <div class="random-track" @click="playRandom">
           <svg width="20" height="20" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M4 23.9733L7.4 23.9867C8.61333 23.9867 9.74667 23.3867 10.4133 22.3867L18.9333 9.61332C19.2632 9.11738 19.7115 8.71142 20.2376 8.43208C20.7637 8.15274 21.351 8.00882 21.9467 8.01332L28.0133 8.03999M25.3333 26.64L28 23.9733M11.8533 11.4933L10.4133 9.49332C10.0803 9.0271 9.63983 8.64798 9.12924 8.38803C8.61864 8.12809 8.05293 7.99499 7.48 7.99999L4 8.01332M17.2933 20.5067L18.92 22.6C19.6 23.48 20.6667 24 21.7867 24L28.0133 23.9733M28 8.02665L25.3333 5.35999" stroke="black" stroke-opacity="0.7" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
@@ -181,20 +254,22 @@ export default{
         </div>
 
         <!-- play / pause  -->
-        <div class="playpause-track" v-if="isTimerPlaying" @click="pauseTrack">
-          <svg width="30" height="30" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect width="50" height="50" rx="25" fill="#C493E1"/>
-            <path d="M19.3041 33.2143C19.3041 33.4257 19.3085 33.6363 19.3128 33.8377L19.3129 33.8444C19.3172 34.0453 19.3212 34.2365 19.3212 34.4207C19.3212 34.798 19.3036 35.1123 19.2474 35.3647C19.1918 35.6148 19.1085 35.7574 19.0126 35.8413C18.9226 35.92 18.7611 36 18.4348 36C18.0271 36 17.8017 35.9123 17.6703 35.8154C17.5421 35.7207 17.4456 35.5746 17.3796 35.3386C17.3114 35.0947 17.2852 34.7879 17.2804 34.4143C17.2776 34.1967 17.2813 33.9929 17.2853 33.7784C17.2886 33.6022 17.292 33.4188 17.292 33.2143L17.292 16.7857C17.292 16.5813 17.2886 16.3978 17.2853 16.2217C17.2813 16.0071 17.2776 15.8034 17.2804 15.5857C17.2852 15.2121 17.3114 14.9053 17.3796 14.6614C17.4456 14.4254 17.5421 14.2793 17.6704 14.1846C17.8017 14.0877 18.0271 14 18.4348 14C18.8463 14 19.0396 14.0891 19.1353 14.168C19.227 14.2438 19.3012 14.3697 19.3417 14.6099C19.3835 14.8578 19.3798 15.1675 19.3591 15.5525C19.3543 15.6423 19.3485 15.7365 19.3426 15.8339C19.3242 16.1333 19.3041 16.4623 19.3041 16.7857V33.2143ZM30.7084 33.2143V16.7857C30.7084 16.5205 30.6972 16.2404 30.6868 15.9802C30.681 15.834 30.6754 15.6941 30.6722 15.5667C30.6627 15.1866 30.6718 14.8782 30.725 14.6324C30.7763 14.3946 30.8593 14.2598 30.9666 14.1756C31.0775 14.0884 31.285 14 31.6947 14C32.1044 14 32.3118 14.0884 32.4228 14.1756C32.5301 14.2598 32.613 14.3946 32.6644 14.6324C32.7176 14.8782 32.7267 15.1866 32.7172 15.5667C32.714 15.6941 32.7084 15.834 32.7026 15.9802C32.6922 16.2404 32.681 16.5205 32.681 16.7857L32.681 33.2143C32.681 33.4795 32.6922 33.7596 32.7026 34.0198C32.7084 34.166 32.714 34.3059 32.7172 34.4333C32.7267 34.8134 32.7176 35.1218 32.6644 35.3676C32.613 35.6054 32.5301 35.7402 32.4228 35.8244C32.3118 35.9116 32.1044 36 31.6947 36C31.285 36 31.0775 35.9116 30.9666 35.8244C30.8593 35.7402 30.7763 35.6054 30.725 35.3676C30.6718 35.1218 30.6627 34.8134 30.6722 34.4333C30.6754 34.3059 30.681 34.166 30.6868 34.0198C30.6972 33.7596 30.7084 33.4795 30.7084 33.2143Z" fill="#E5E5E5" stroke="#E5E5E5"/>
-          </svg>
+          <div @click="play">
+            <button v-if="isTimerPlaying">
+              <svg width="30" height="30" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="50" height="50" rx="25" fill="#C493E1"/>
+                <path d="M19.3041 33.2143C19.3041 33.4257 19.3085 33.6363 19.3128 33.8377L19.3129 33.8444C19.3172 34.0453 19.3212 34.2365 19.3212 34.4207C19.3212 34.798 19.3036 35.1123 19.2474 35.3647C19.1918 35.6148 19.1085 35.7574 19.0126 35.8413C18.9226 35.92 18.7611 36 18.4348 36C18.0271 36 17.8017 35.9123 17.6703 35.8154C17.5421 35.7207 17.4456 35.5746 17.3796 35.3386C17.3114 35.0947 17.2852 34.7879 17.2804 34.4143C17.2776 34.1967 17.2813 33.9929 17.2853 33.7784C17.2886 33.6022 17.292 33.4188 17.292 33.2143L17.292 16.7857C17.292 16.5813 17.2886 16.3978 17.2853 16.2217C17.2813 16.0071 17.2776 15.8034 17.2804 15.5857C17.2852 15.2121 17.3114 14.9053 17.3796 14.6614C17.4456 14.4254 17.5421 14.2793 17.6704 14.1846C17.8017 14.0877 18.0271 14 18.4348 14C18.8463 14 19.0396 14.0891 19.1353 14.168C19.227 14.2438 19.3012 14.3697 19.3417 14.6099C19.3835 14.8578 19.3798 15.1675 19.3591 15.5525C19.3543 15.6423 19.3485 15.7365 19.3426 15.8339C19.3242 16.1333 19.3041 16.4623 19.3041 16.7857V33.2143ZM30.7084 33.2143V16.7857C30.7084 16.5205 30.6972 16.2404 30.6868 15.9802C30.681 15.834 30.6754 15.6941 30.6722 15.5667C30.6627 15.1866 30.6718 14.8782 30.725 14.6324C30.7763 14.3946 30.8593 14.2598 30.9666 14.1756C31.0775 14.0884 31.285 14 31.6947 14C32.1044 14 32.3118 14.0884 32.4228 14.1756C32.5301 14.2598 32.613 14.3946 32.6644 14.6324C32.7176 14.8782 32.7267 15.1866 32.7172 15.5667C32.714 15.6941 32.7084 15.834 32.7026 15.9802C32.6922 16.2404 32.681 16.5205 32.681 16.7857L32.681 33.2143C32.681 33.4795 32.6922 33.7596 32.7026 34.0198C32.7084 34.166 32.714 34.3059 32.7172 34.4333C32.7267 34.8134 32.7176 35.1218 32.6644 35.3676C32.613 35.6054 32.5301 35.7402 32.4228 35.8244C32.3118 35.9116 32.1044 36 31.6947 36C31.285 36 31.0775 35.9116 30.9666 35.8244C30.8593 35.7402 30.7763 35.6054 30.725 35.3676C30.6718 35.1218 30.6627 34.8134 30.6722 34.4333C30.6754 34.3059 30.681 34.166 30.6868 34.0198C30.6972 33.7596 30.7084 33.4795 30.7084 33.2143Z" fill="#E5E5E5" stroke="#E5E5E5"/>
+              </svg>
+            </button>
 
-        </div>
+            <button v-else>
+              <svg width="30" height="30" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="50" height="50" rx="25" fill="#C493E1"/>
+                <path d="M23.1398 15.4316L23.1402 15.4318L27.5787 17.8336L32.0166 20.2351C32.0168 20.2352 32.0169 20.2353 32.0171 20.2354C34.6396 21.657 35.75 23.4178 35.75 25C35.75 26.5823 34.6395 28.3432 32.0166 29.7649L27.5787 32.1664L23.1402 34.5682L23.1398 34.5684C20.5247 35.9859 18.3068 36.0327 16.7934 35.2113C15.2957 34.3984 14.25 32.6039 14.25 29.8036V25V20.1964C14.25 17.3961 15.2957 15.6016 16.7934 14.7887C18.3068 13.9673 20.5247 14.0141 23.1398 15.4316Z" stroke="#E5E5E5" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+            </button>
+          </div>
 
-        <div v-else @click="playTrack">
-          <svg width="30" height="30" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect width="50" height="50" rx="25" fill="#C493E1"/>
-            <path d="M23.1398 15.4316L23.1402 15.4318L27.5787 17.8336L32.0166 20.2351C32.0168 20.2352 32.0169 20.2353 32.0171 20.2354C34.6396 21.657 35.75 23.4178 35.75 25C35.75 26.5823 34.6395 28.3432 32.0166 29.7649L27.5787 32.1664L23.1402 34.5682L23.1398 34.5684C20.5247 35.9859 18.3068 36.0327 16.7934 35.2113C15.2957 34.3984 14.25 32.6039 14.25 29.8036V25V20.1964C14.25 17.3961 15.2957 15.6016 16.7934 14.7887C18.3068 13.9673 20.5247 14.0141 23.1398 15.4316Z" stroke="#E5E5E5" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-          </svg>
-        </div>
 
         <!-- next track -->
         <div class="next-track" @click="nextTrack">
@@ -204,7 +279,7 @@ export default{
         </div>
 
         <!-- loop track -->
-        <div class="repeat-track" @click="playLoop" >
+        <div class="repeat-track" @click="playLoop">
           <svg width="20" height="20" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M6.54398 6.88H23.2267C25.44 6.88 27.2267 8.66667 27.2267 10.88V15.3067" stroke="black" stroke-opacity="0.7" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="M8.98666 2.66669L4.77333 6.88002L8.98666 11.0934M27.2267 25.12H8.77333C6.56 25.12 4.77333 23.3334 4.77333 21.12V16.6934" stroke="black" stroke-opacity="0.7" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
