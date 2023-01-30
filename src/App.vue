1a14aec744e6b3f1a572a9c2d92030a0bca0acd1
Bowonwit Anothaisintavee <Bowonwit.A@gmail.com>
2023-01-30 22:44:05 +0700

    fix: debug the music player card

--- src/App.vue
+++ src/App.vue
@@ -325,8 +325,8 @@ export default {
       <!-- Header & Playlist -->
       <div class="h-1/3">
         <!-- Header -->
-        <div class="grid grid-cols-12 pb-3">
-          <h1 class="text-2xl font-bold text-white">YourStyle</h1>
+        <div class="grid grid-cols-2 pb-3">
+          <h1 class="text-2xl font-bold text-white col-span-2">Your Style</h1>
           <div class="col-end-13 flex justify-end gap-2">
             <!-- Next & Previous Icon -->
             <svg
@@ -391,28 +391,25 @@ export default {
           </div>
         </div>
       </div>
-      <!-- MusicPlaying & Trending -->
+      <!-- Music Player & Trending -->
       <div class="h-2/3">
-        <!-- Music Playing -->
+        <!-- Music Player -->
         <div class="grid grid-cols-12 gap-5 h-full">
           <div class="flex flex-col col-span-3">
             <h1 class="text-2xl font-bold pb-1.5 text-white">Now Playing</h1>
-            <div class="grow flex flex-col bg-[#E5E5E5] rounded-2xl h-[25rem]">
-              <div
-                class="grow h-3/5 bg-cover rounded-t-2xl"
-                :style="{ backgroundImage: `url(${tracks[0].cover})` }"
-              ></div>
+            <div class="grow flex flex-col bg-[#E5E5E5] rounded-2xl h-[23rem]" >
+              <div class="grow h-3/5 bg-cover rounded-t-2xl" :style="{'backgroundImage': 'url(' + encodeURI(currentTrack.cover) + ')'}"></div>
               <div
                 class="flex flex-col justify-center items-center gap-2 h-2/5 bg-[#E5E5E5] rounded-b-2xl"
               >
                 <!-- Title Name -->
                 <div class="pt-2 text-center">
-                  <h1 class="text-2xl font-bold">{{ tracks[0].name }}</h1>
-                  <h3 class="font-semibold">{{ tracks[0].artist }}</h3>
+                  <h1 class="text-2xl font-bold">{{ currentTrack.name }}</h1>
+                  <h3 class="font-semibold">{{ currentTrack.artist }}</h3>
                 </div>
                 <!-- Time Counter -->
                 <div class="flex w-fit items-center">
-                  <p class="px-2 text-sm">00:00</p>
+                  <p class="px-2 text-sm">{{ currentTime }}</p>
                   <div>
                     <svg
                       width="139"
@@ -440,124 +437,84 @@ export default {
                     </svg>
                   </div>
 
-                  <p class="px-2 text-sm">00:00</p>
+                  <p class="px-2 text-sm">{{ duration }}</p>
                 </div>
                 <!-- Icons -->
                 <div class="flex justify-around items-center gap-3 pb-4">
                   <!-- Shuffle Icon -->
-                  <div>
-                    <svg
-                      width="32"
-                      height="32"
-                      viewBox="0 0 32 32"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M4 23.9733L7.4 23.9867C8.61333 23.9867 9.74667 23.3867 10.4133 22.3867L18.9333 9.61332C19.2632 9.11738 19.7115 8.71142 20.2376 8.43208C20.7637 8.15274 21.351 8.00882 21.9467 8.01332L28.0133 8.03999M25.3333 26.64L28 23.9733M11.8533 11.4933L10.4133 9.49332C10.0803 9.0271 9.63983 8.64798 9.12924 8.38803C8.61864 8.12809 8.05293 7.99499 7.48 7.99999L4 8.01332M17.2933 20.5067L18.92 22.6C19.6 23.48 20.6667 24 21.7867 24L28.0133 23.9733M28 8.02665L25.3333 5.35999"
-                        stroke="#171717"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                    </svg>
+                  <div class="random-track" @click="playRandom">
+                    <button>
+                      <svg width="20" height="20" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M4 23.9733L7.4 23.9867C8.61333 23.9867 9.74667 23.3867 10.4133 22.3867L18.9333 9.61332C19.2632 9.11738 19.7115 8.71142 20.2376 8.43208C20.7637 8.15274 21.351 8.00882 21.9467 8.01332L28.0133 8.03999M25.3333 26.64L28 23.9733M11.8533 11.4933L10.4133 9.49332C10.0803 9.0271 9.63983 8.64798 9.12924 8.38803C8.61864 8.12809 8.05293 7.99499 7.48 7.99999L4 8.01332M17.2933 20.5067L18.92 22.6C19.6 23.48 20.6667 24 21.7867 24L28.0133 23.9733M28 8.02665L25.3333 5.35999"
+                            stroke="black" stroke-opacity="0.7" stroke-width="1.5" stroke-linecap="round"
+                            stroke-linejoin="round"/>
+                      </svg>
+                    </button>
                   </div>
                   <!-- Previous Icon -->
-                  <div>
-                    <svg
-                      width="33"
-                      height="32"
-                      viewBox="0 0 33 32"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M5.26336 7.77156L5.26336 24.2516M27.2367 22.3849L27.2367 9.62489C27.2367 7.01156 24.3967 5.37156 22.13 6.67823L16.5967 9.86489L11.0634 13.0649C8.79669 14.3716 8.79669 17.6382 11.0634 18.9449L16.5967 22.1449L22.13 25.3316C24.3967 26.6382 27.2367 25.0116 27.2367 22.3849Z"
-                        stroke="#171717"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                    </svg>
+                  <div class="prev-track" @click="prevTrack">
+                    <button>
+                      <svg width="20" height="20" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M5.01336 7.77156L5.01336 24.2516M26.9867 22.3849L26.9867 9.62489C26.9867 7.01156 24.1467 5.37156 21.88 6.67823L16.3467 9.86489L10.8134 13.0649C8.54669 14.3716 8.54669 17.6382 10.8134 18.9449L16.3467 22.1449L21.88 25.3316C24.1467 26.6382 26.9867 25.0116 26.9867 22.3849Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                      </svg>
+                    </button>
                   </div>
                   <!-- Play/Pause Icon -->
-                  <div>
-                    <svg
-                      width="51"
-                      height="50"
-                      viewBox="0 0 51 50"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <rect
-                        x="0.5"
-                        width="50"
-                        height="50"
-                        rx="25"
-                        fill="#C493E1"
-                      />
-                      <path
-                        d="M23.6402 15.4318L23.6398 15.4316C21.0247 14.0141 18.8068 13.9673 17.2934 14.7887C15.7957 15.6016 14.75 17.3961 14.75 20.1964V25V29.8036C14.75 32.6039 15.7957 34.3984 17.2934 35.2113C18.8068 36.0327 21.0247 35.9859 23.6398 34.5684L23.6402 34.5682L28.0787 32.1664L32.5166 29.7649L23.6402 15.4318ZM23.6402 15.4318L28.0787 17.8336M23.6402 15.4318L28.0787 17.8336M28.0787 17.8336L32.5166 20.2351M28.0787 17.8336L32.5166 20.2351M32.5166 20.2351C35.1395 21.6568 36.25 23.4177 36.25 25C36.25 26.5822 35.1396 28.343 32.5171 29.7646L32.5166 20.2351Z"
-                        stroke="#E5E5E5"
-                        stroke-width="1.5"
-                        stroke-miterlimit="10"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                    </svg>
+                  <div @click="play">
+                    <button v-if="isTimerPlaying">
+                      <svg width="30" height="30" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="50" height="50" rx="25" fill="#C493E1"/>
+                        <path d="M19.3041 33.2143C19.3041 33.4257 19.3085 33.6363 19.3128 33.8377L19.3129 33.8444C19.3172 34.0453 19.3212 34.2365 19.3212 34.4207C19.3212 34.798 19.3036 35.1123 19.2474 35.3647C19.1918 35.6148 19.1085 35.7574 19.0126 35.8413C18.9226 35.92 18.7611 36 18.4348 36C18.0271 36 17.8017 35.9123 17.6703 35.8154C17.5421 35.7207 17.4456 35.5746 17.3796 35.3386C17.3114 35.0947 17.2852 34.7879 17.2804 34.4143C17.2776 34.1967 17.2813 33.9929 17.2853 33.7784C17.2886 33.6022 17.292 33.4188 17.292 33.2143L17.292 16.7857C17.292 16.5813 17.2886 16.3978 17.2853 16.2217C17.2813 16.0071 17.2776 15.8034 17.2804 15.5857C17.2852 15.2121 17.3114 14.9053 17.3796 14.6614C17.4456 14.4254 17.5421 14.2793 17.6704 14.1846C17.8017 14.0877 18.0271 14 18.4348 14C18.8463 14 19.0396 14.0891 19.1353 14.168C19.227 14.2438 19.3012 14.3697 19.3417 14.6099C19.3835 14.8578 19.3798 15.1675 19.3591 15.5525C19.3543 15.6423 19.3485 15.7365 19.3426 15.8339C19.3242 16.1333 19.3041 16.4623 19.3041 16.7857V33.2143ZM30.7084 33.2143V16.7857C30.7084 16.5205 30.6972 16.2404 30.6868 15.9802C30.681 15.834 30.6754 15.6941 30.6722 15.5667C30.6627 15.1866 30.6718 14.8782 30.725 14.6324C30.7763 14.3946 30.8593 14.2598 30.9666 14.1756C31.0775 14.0884 31.285 14 31.6947 14C32.1044 14 32.3118 14.0884 32.4228 14.1756C32.5301 14.2598 32.613 14.3946 32.6644 14.6324C32.7176 14.8782 32.7267 15.1866 32.7172 15.5667C32.714 15.6941 32.7084 15.834 32.7026 15.9802C32.6922 16.2404 32.681 16.5205 32.681 16.7857L32.681 33.2143C32.681 33.4795 32.6922 33.7596 32.7026 34.0198C32.7084 34.166 32.714 34.3059 32.7172 34.4333C32.7267 34.8134 32.7176 35.1218 32.6644 35.3676C32.613 35.6054 32.5301 35.7402 32.4228 35.8244C32.3118 35.9116 32.1044 36 31.6947 36C31.285 36 31.0775 35.9116 30.9666 35.8244C30.8593 35.7402 30.7763 35.6054 30.725 35.3676C30.6718 35.1218 30.6627 34.8134 30.6722 34.4333C30.6754 34.3059 30.681 34.166 30.6868 34.0198C30.6972 33.7596 30.7084 33.4795 30.7084 33.2143Z" fill="#E5E5E5" stroke="#E5E5E5"/>
+                      </svg>
+                    </button>
+
+                    <button v-else>
+                      <svg width="30" height="30" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="50" height="50" rx="25" fill="#C493E1"/>
+                        <path d="M23.1398 15.4316L23.1402 15.4318L27.5787 17.8336L32.0166 20.2351C32.0168 20.2352 32.0169 20.2353 32.0171 20.2354C34.6396 21.657 35.75 23.4178 35.75 25C35.75 26.5823 34.6395 28.3432 32.0166 29.7649L27.5787 32.1664L23.1402 34.5682L23.1398 34.5684C20.5247 35.9859 18.3068 36.0327 16.7934 35.2113C15.2957 34.3984 14.25 32.6039 14.25 29.8036V25V20.1964C14.25 17.3961 15.2957 15.6016 16.7934 14.7887C18.3068 13.9673 20.5247 14.0141 23.1398 15.4316Z" stroke="#E5E5E5" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                      </svg>
+                    </button>
                   </div>
                   <!-- Skip Icon -->
-                  <div>
-                    <svg
-                      width="33"
-                      height="32"
-                      viewBox="0 0 33 32"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M27.7366 24.24L27.7366 7.76004M5.76331 9.6267L5.76331 22.3867C5.76331 25 8.60331 26.64 10.87 25.3334L16.4033 22.1467L21.9366 18.9467C24.2033 17.64 24.2033 14.3734 21.9366 13.0667L16.4033 9.8667L10.87 6.68004C8.60331 5.37337 5.76331 7.00004 5.76331 9.6267Z"
-                        stroke="#171717"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                    </svg>
+                  <div class="next-track" @click="nextTrack">
+                    <button>
+                      <svg width="20" height="20" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M26.9867 24.24L26.9867 7.75998M5.01334 9.62664L5.01334 22.3866C5.01334 25 7.85334 26.64 10.12 25.3333L15.6533 22.1466L21.1867 18.9466C23.4533 17.64 23.4533 14.3733 21.1867 13.0666L15.6533 9.86664L10.12 6.67998C7.85334 5.37331 5.01334 6.99998 5.01334 9.62664Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                      </svg>
+                    </button>
                   </div>
                   <!-- Loop Icon -->
-                  <div>
-                    <svg
-                      width="32"
-                      height="32"
-                      viewBox="0 0 32 32"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M6.54401 6.88H23.2267C25.44 6.88 27.2267 8.66667 27.2267 10.88V15.3067"
-                        stroke="#171717"
-                        stroke-width="1.5"
-                        stroke-miterlimit="10"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                      <path
-                        d="M8.98665 2.66663L4.77332 6.87996L8.98665 11.0933M27.2266 25.12H8.77332C6.55998 25.12 4.77332 23.3333 4.77332 21.12V16.6933"
-                        stroke="#171717"
-                        stroke-width="1.5"
-                        stroke-miterlimit="10"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                      <path
-                        d="M23.0133 29.3333L27.2266 25.1199L23.0133 20.9066"
-                        stroke="#171717"
-                        stroke-width="1.5"
-                        stroke-miterlimit="10"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                    </svg>
+                  <div class="repeat-track">
+                    <button v-if="!repeat">
+                      <svg width="20" height="20" viewBox="0 0 32 32" fill="none"
+                           xmlns="http://www.w3.org/2000/svg" @click="playLoop">
+                        <path d="M6.54398 6.88H23.2267C25.44 6.88 27.2267 8.66667 27.2267 10.88V15.3067" stroke="black"
+                              stroke-opacity="0.7" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                        <path
+                            d="M8.98666 2.66669L4.77333 6.88002L8.98666 11.0934M27.2267 25.12H8.77333C6.56 25.12 4.77333 23.3334 4.77333 21.12V16.6934"
+                            stroke="black" stroke-opacity="0.7" stroke-width="1.5" stroke-miterlimit="10"
+                            stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M23.0133 29.3333L27.2267 25.12L23.0133 20.9067" stroke="black" stroke-opacity="0.7"
+                              stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                      </svg>
+                    </button>
+
+                    <button v-else>
+                      <svg  width="20" height="20" viewBox="0 0 32 32" fill="none"
+                           xmlns="http://www.w3.org/2000/svg" @click="playLoop">
+                        <path d="M6.54398 6.88H23.2267C25.44 6.88 27.2267 8.66667 27.2267 10.88V15.3067"
+                              stroke="#C493E1" stroke-width="2.5" stroke-miterlimit="10" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                        <path
+                            d="M8.98668 2.66667L4.77335 6.88L8.98668 11.0933M27.2267 25.12H8.77335C6.56001 25.12 4.77335 23.3333 4.77335 21.12V16.6933"
+                            stroke="#C493E1" stroke-width="2.5" stroke-miterlimit="10" stroke-linecap="round"
+                            stroke-linejoin="round"/>
+                        <path d="M23.0133 29.3333L27.2267 25.12L23.0133 20.9067" stroke="#C493E1" stroke-width="2.5"
+                              stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                      </svg>
+                    </button>
                   </div>
                 </div>
               </div>
@@ -571,7 +528,7 @@ export default {
               <!-- for-loop here -->
               <div
                 class="flex items-center mb-2 h-fit bg-[#E5E5E5] hover:bg-gray-300 transition ease-in-out rounded-2xl"
-                v-for="(tracks, index) in tracks"
+                v-for="(track, index) in tracks"
                 :key="index"
               >
                 <!-- Soung Count -->
@@ -580,15 +537,15 @@ export default {
                 </div>
                 <!-- Song Cover -->
                 <div class="w-14">
-                  <img :src="tracks.cover" />
+                  <img :src="track.cover"/>
                 </div>
                 <!-- Title & Artist -->
                 <div class="grow grid grid-rows-2 h-fit pl-5">
                   <h1 class="row-span-1 text-xl font-bold">
-                    {{ tracks.name }}
+                    {{ track.name }}
                   </h1>
                   <h1 class="row-span-1 font-semibold">
-                    {{ tracks.artist }}
+                    {{ track.artist }}
                   </h1>
                 </div>
                 <!-- Time Counter -->
