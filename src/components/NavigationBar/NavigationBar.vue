6965a97d58019a7aea9a14cc2ddc19bbbd056fd8
PhuMiZz <siripoom.kuso@kmutt.ac.th>
2023-03-02 22:01:30 +0700

    Create HomeContainer&NavBar Components

---
+++ src/components/NavigationBar/NavigationBar.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HomePageButton from '@/components/icon/NavigationBar/HomePageButton.vue'
+import SearchPageButton from '@/components/icon/NavigationBar/SearchPageButton.vue'
+import PlaylistPageButton from '@/components/icon/NavigationBar/PlaylistPageButton.vue'
+import CreditPageButton from '@/components/icon/NavigationBar/CreditPageButton.vue'
+import SettingPageButton from '@/components/icon/NavigationBar/SettingPageButton.vue'
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped></style>
