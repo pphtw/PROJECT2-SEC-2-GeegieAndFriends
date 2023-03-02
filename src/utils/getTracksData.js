11785866cfdb4f332fa141f8afb05af8be112b8a
Bowonwit Anothaisintavee <Bowonwit.A@gmail.com>
2023-03-02 22:09:55 +0700

    restructure files

---
+++ src/utils/getTracksData.js
@@ -0,0 +1,18 @@
+import metadata from '../assets/metadata.json'
+
+const playlistData = metadata.playlists
+const trackData = metadata.tracks
+
+export const getTrack = (trackId = 1) => {
+  return trackData.find((track) => track['trackId'] === trackId)
+}
+export const getTrackIdList = (playlistId) => {
+  return [
+    ...playlistData.find(
+      (playlist) => playlist['playlistId'] === Number(playlistId)
+    ).tracks,
+  ]
+}
+export const getPlaylist = (playlistId) => {
+  return playlistData.find((playlist) => playlist['playlistId'] === playlistId)
+}
