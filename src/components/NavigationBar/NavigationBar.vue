06226b79e220bcde4d4d150fb0ad294823f63093
PhuMiZz <siripoom.kuso@kmutt.ac.th>
2023-03-02 22:17:53 +0700

    Move HomePage and Navbar to components

--- src/components/NavigationBar/NavigationBar.vue
+++ src/components/NavigationBar/NavigationBar.vue
@@ -1,13 +1,26 @@
 <script setup>
-import HomePageButton from '@/components/icon/NavigationBar/HomePageButton.vue'
-import SearchPageButton from '@/components/icon/NavigationBar/SearchPageButton.vue'
-import PlaylistPageButton from '@/components/icon/NavigationBar/PlaylistPageButton.vue'
-import CreditPageButton from '@/components/icon/NavigationBar/CreditPageButton.vue'
-import SettingPageButton from '@/components/icon/NavigationBar/SettingPageButton.vue'
+import HomePageButton from './Icons/HomePageButton.vue'
+import SearchPageButton from './Icons/SearchPageButton.vue'
+import PlaylistPageButton from './Icons/PlaylistPageButton.vue'
+import CreditPageButton from './Icons/CreditPageButton.vue'
+import SettingPageButton from './Icons/SettingPageButton.vue'
 </script>
 
 <template>
-  <div></div>
+  <div
+    class="flex flex-row order-2 sm:order-1 sm:flex-col justify-center row-span-6 gap-5 items-center w-full sm:w-[5.4%] py-3 sm:py-0 h-fit sm:h-full max-sm:z-10 bg-[#162750]"
+  >
+    <!-- #HomePageButton -->
+    <HomePageButton />
+    <!-- #SearchPageButton -->
+    <SearchPageButton />
+    <!-- #PlaylistPageButton -->
+    <PlaylistPageButton />
+    <!-- #CreditPageButton -->
+    <CreditPageButton />
+    <!-- #SettingPageButton -->
+    <SettingPageButton />
+  </div>
 </template>
 
 <style scoped></style>
