ea983950329158dfaefb2fbffd5106ee3c20a320
pphtw <phutawan.pram@gmail.com>
2023-03-06 14:51:50 +0700

    [FIX] Music Player Component

--- src/components/MusicPlayerCard/MusicPlayerCard.vue
+++ src/components/MusicPlayerCard/MusicPlayerCard.vue
@@ -11,37 +11,7 @@ import { ref, reactive ,inject } from 'vue'
 
 const audioElement = inject('audioElement')
 const musicQueue = inject('musicQueue')
-
-//progress bar
-const progressBar = reactive({
-  barWidth: '0%',
-  isClicked: false,
-  currentTime: '00:00',
-  duration: '00:00',
-  boundingRect: new DOMRect(),
-  newTime: 0,
-  updateProgressBar: () => {
-    progressBar.barWidth =
-      (audioElement.value.currentTime / audioElement.value.duration) * 100 + '%'
-  },
-  updateTime: (e) => {
-    const x = progressBar.validateX(e.clientX)
-    progressBar.newTime =
-      (x / progressBar.boundingRect.width) * audioElement.value.duration
-    progressBar.barWidth = (x / progressBar.boundingRect.width) * 100 + '%'
-  },
-  validateX: (x) => {
-    // clientX is a property of the event object in JavaScript
-    // progressBar.boundingRect.width = width of progress bar
-    if (x < progressBar.boundingRect.left) {
-      return 0
-    } else if (x > progressBar.boundingRect.right) {
-      return progressBar.boundingRect.width + 2
-    } else {
-      return x - progressBar.boundingRect.left
-    }
-  },
-})
+const progressBar = inject('progressBar')
 //DOM Element
 const progressBarElement = ref(null)
 
@@ -59,18 +29,7 @@ const trackSkipHandler = (toNext = true) => {
   musicQueue.skipTrack(toNext)
   toggleDelayedPlayPause()
 }
-const onLoadMetadataHandler = () => {
-  progressBar.duration = secToMin(audioElement.value.duration)
-  progressBar.currentTime = secToMin(audioElement.value.currentTime)
-  progressBar.updateProgressBar()
-  isOverflowed()
-}
-const onTimeUpdateHandler = () => {
-  progressBar.currentTime = secToMin(audioElement.value.currentTime)
-  if (!progressBar.isClicked) {
-    progressBar.updateProgressBar()
-  }
-}
+
 const toggleDelayedPlayPause = (delay = 0) => {
   setTimeout(() => {
     if (musicQueue.isPlaying) {
