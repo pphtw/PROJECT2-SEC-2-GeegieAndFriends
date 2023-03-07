ba78f5cfe11f838b49fd7ad9e7c0e0556ed677dc
Banlearit <banlearit.siri@gmail.com>
2023-03-08 00:38:56 +0700

    create new Timer component

---
+++ src/components/UI/atoms/Timer.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+  time:{
+    type: String,
+    required: true
+  },
+  size:{
+    type:Number,
+    required: false
+  }
+})
+const getFontSize = (n = 1) => {
+  switch (n) {
+    case 1 : return 'text-sm'
+    case 2 : return 'text-md'
+    case 3 : return 'text-lg'
+    case 4 : return 'text-xl'
+  }
+}
+</script>
+
+<template>
+    <p class="px-2" :class="getFontSize(size)">{{ time }}</p>
+</template>
+
+<style scoped>
+
+</style>
