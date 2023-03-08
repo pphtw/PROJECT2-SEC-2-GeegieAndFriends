39d25048ee264b3c2ac547ac5107635e4ee20e25
PhuMiZz <siripoom.kuso@kmutt.ac.th>
2023-03-08 14:24:09 +0700

    fix grid layout

--- src/components/templates/ContentSection.vue
+++ src/components/templates/ContentSection.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div
-    class="col-span-1 row-span-1 max-sm:w-full sm:row-auto sm:flex sm:flex-col sm:justify-start sm:h-full max-sm:place-self-center"
+    class="row-span-1 max-sm:w-full sm:row-auto sm:flex sm:flex-col sm:justify-start sm:h-full max-sm:place-self-center"
   >
     <slot name="header"></slot>
     <slot></slot>
