43df9e1369a3fc2d76163d4f81c3d10bcb650a56
Bowonwit Anothaisintavee <Bowonwit.A@gmail.com>
2023-03-08 10:52:06 +0700

    create template for section content

---
+++ src/components/templates/ContentSection.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div
+    class="col-span-1 row-span-1 max-sm:w-full sm:row-auto sm:flex sm:flex-col sm:justify-start sm:h-full max-sm:place-self-center"
+  >
+    <slot name="header"></slot>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped></style>
