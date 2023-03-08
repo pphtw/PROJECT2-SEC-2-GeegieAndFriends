2587d77f289617dc1dbdd06094b9bc861a16b523
Bowonwit Anothaisintavee <Bowonwit.A@gmail.com>
2023-03-08 17:17:02 +0700

    fix some layout

--- src/components/templates/ContentSection.vue
+++ src/components/templates/ContentSection.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="w-full h-full bg-gray-500">
+  <div class="w-full h-full bg-gray-500 flex flex-col gap-4">
     <slot name="header"></slot>
     <slot></slot>
   </div>
