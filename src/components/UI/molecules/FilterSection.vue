7b8f2a1e426d23ebb27e5a2f4f7d0b540348aef4
pphtw <phutawan.palakavong@gmail.kmutt.ac.th>
2023-03-11 16:19:03 +0700

    [ADD] filterSection

---
+++ src/components/UI/molecules/FilterSection.vue
@@ -0,0 +1,21 @@
+<script setup>
+import FilterButton from '../atoms/FilterButton.vue';
+
+</script>
+ 
+<template>
+    <div class="flex gap-2 py-4 radioFilter">
+        <FilterButton filterName="All"/>
+        <FilterButton filterName="Playlists"/>
+        <FilterButton filterName="Artists"/>
+        <FilterButton filterName="Profile"/>
+        <FilterButton filterName="Songs"/>
+        <FilterButton filterName="Albums"/>
+    </div>
+</template>
+ 
+<style scoped>
+.radioFilter:checked + label{
+    background-color: #C493E1;
+}
+</style>
--- src/components/templates/SearchPageTemplate.vue
+++ src/components/templates/SearchPageTemplate.vue
@@ -1,9 +1,5 @@
 <template>
-    <!-- <div class="bg-[#162750] text-white rounded-full py-1 px-2 self-center">
-        <slot name="filter">
-            
-        </slot>
-    </div> -->
+
 </template>
 
 <script setup>
