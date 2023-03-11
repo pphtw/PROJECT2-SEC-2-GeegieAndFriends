ee912653cf410c22136dd0deb616a0303c90e4c8
pphtw <phutawan.palakavong@gmail.kmutt.ac.th>
2023-03-11 13:31:00 +0700

    [ADD] Search Page Layout

--- src/components/pages/SearchPage.vue
+++ src/components/pages/SearchPage.vue
@@ -1,8 +1,10 @@
 <script setup>
-    import SingleTrack from '../UI/organisms/SingleTrack.vue';
-    import MusicPlayerCard from '../UI/organisms/MusicPlayerCard.vue';
-    import NavigationBar from '../UI/organisms/NavigationBar.vue';
-    import SearchIcon from '../UI/atoms/SearchPageButton.vue'
+import SingleTrack from '../UI/organisms/SingleTrack.vue';
+import MusicPlayerCard from '../UI/organisms/MusicPlayerCard.vue';
+import NavigationBar from '../UI/organisms/NavigationBar.vue';
+import PlaylistCarousel from '../UI/organisms/PlaylistCarousel.vue';
+import FilterSection from '../UI/atoms/FilterSection.vue';
+import SearchBar from '../UI/molecules/SearchBar.vue';
 </script>
  
 <template>
@@ -10,38 +12,41 @@
     class="w-screen h-screen flex flex-row bg-[#3f5586]"
 >
 <NavigationBar/>
-    <div class="w-full px-[4vw] py-8 gap-y-8 gap-x-[4vw] grid grid-cols-2 grid-rows-3">
-    <!-- #SearchBarSection -->
-    <div class="h-10 flex rounded-full bg-[#E5E5E5] gap-3 px-3 py-1.5">
-        <SearchIcon 
-            stroke="black"
-            class="flex-none self-center"
-        />
-        <input
-            class="bg-transparent flex-1" 
-            type="text w-full" 
-            placeholder="What do you want to listen to?"/>
-    </div>
+    <div class="w-full px-[4vw] py-8 gap-y-8 gap-x-[4vw] grid grid-cols-4">
+       <div class="bg-slate-500 col-span-3 gap-y-3">
+        <!-- #SearchBarSection -->
+        <SearchBar/>
 
-    <!-- #FilterSection -->
-    <div class="bg-slate-500">
+        <!-- #FilterSection -->
+        <div class="bg-slate-500 flex gap-2 py-4">
+            <FilterSection filterName="All"/>
+            <FilterSection filterName="Playlists"/>
+            <FilterSection filterName="Artists"/>
+            <FilterSection filterName="Profile"/>
+            <FilterSection filterName="Songs"/>
+            <FilterSection filterName="Albums"/>
+        </div>
+
+        <!-- #TrackSection -->
+        <div class="bg-red-500">
+        <!-- <SingleTrack :playlist="playlist" /> -->
+        </div>
+       </div> 
+       
+       <div class="bg-slate-500">
         
-    </div>
 
-    <!-- #TrackSection -->
-    <div class="bg-slate-500">
-        <!-- <SingleTrack/> -->
-    </div>
+        <!-- #PlaylistSection -->
+        <div class="h-2/5">
+            <PlaylistCarousel/>
+        </div>
 
-    <!-- #PlaylistSection -->
-    <div class="bg-slate-500">
+        <!-- #MusicPlayerCardSection -->
+        <div class="h-3/5">
+            <MusicPlayerCard/>
+        </div>
+       </div>
 
-    </div>
-
-    <!-- #MusicPlayerCardSection -->
-    <div class="bg-slate-500">
-
-    </div>
     </div>
 </div>
 </template>
--- src/components/templates/SearchPageTemplate.vue
+++ src/components/templates/SearchPageTemplate.vue
@@ -1,5 +1,9 @@
 <template>
-
+    <!-- <div class="bg-[#162750] text-white rounded-full py-1 px-2 self-center">
+        <slot name="filter">
+            
+        </slot>
+    </div> -->
 </template>
 
 <script setup>
