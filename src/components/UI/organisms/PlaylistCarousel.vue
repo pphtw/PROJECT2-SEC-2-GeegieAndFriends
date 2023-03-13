ad8030da013b45756fd7da50f9bcb6762a6cd850
PhuMiZz <siripoom.kuso@kmutt.ac.th>
2023-03-14 01:45:15 +0700

    Fix layout Playlist and Trending

--- src/components/UI/organisms/PlaylistCarousel.vue
+++ src/components/UI/organisms/PlaylistCarousel.vue
@@ -14,23 +14,25 @@ const mappedPlaylists = playlistStore.pinnedPlaylist.map((playlistId) =>
 )
 </script>
 <template>
-  <div class="grid grid-cols-3 grid-rows-3 gap-8">
-    <div
-      v-for="playlist in mappedPlaylists"
-      :style="{
-        backgroundImage: 'url(' + encodeURI(playlist.background) + ')',
-      }"
-      :key="playlist['playlistId']"
-      :id="playlist['playlistId']"
-      @click="onChoosePlaylist"
-      class="flex justify-center cursor-pointer hover:opacity-80 bg-cover"
-      tabindex="-1"
-    >
-      <p
-        class="text-white truncate text-lg font-semibold self-center text-center"
+  <div class="h-full overflow-y-auto">
+    <div class="grid grid-cols-3 gap-2 h-fit overflow-auto">
+      <div
+        v-for="playlist in mappedPlaylists"
+        :style="{
+          backgroundImage: 'url(' + encodeURI(playlist.background) + ')',
+        }"
+        :key="playlist['playlistId']"
+        :id="playlist['playlistId']"
+        @click="onChoosePlaylist"
+        class="flex justify-center cursor-pointer h-28 hover:opacity-80 bg-cover rounded-2xl my-auto"
+        tabindex="-1"
       >
-        {{ playlist.name }}
-      </p>
+        <p
+          class="text-white truncate text-lg font-semibold self-center text-center"
+        >
+          {{ playlist.name }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
