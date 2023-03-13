14e6d4a30331da9a360b62d05b55697040881876
pphtw <phutawan.pram@gmail.com>
2023-03-13 21:39:44 +0700

    [EDIT] Filter Section

--- src/components/UI/molecules/FilterSection.vue
+++ src/components/UI/molecules/FilterSection.vue
@@ -1,21 +1,37 @@
 <script setup>
+import { ref } from 'vue';
 import FilterButton from '../atoms/FilterButton.vue';
+
+const filterType = ['All', 'Playlists', 'Profile', 'Artists', 'Song', 'Albums']
+
+const filterHandler = (e) => {
+
+}
 
 </script>
  
 <template>
-    <div class="flex gap-2 py-4 radioFilter">
-        <FilterButton filterName="All"/>
+    <div 
+    class="flex gap-2 py-4 radioFilter"
+    v-for="(filter, index) in filterType"
+    :key="index">
+        <!-- manual -->
+        <!-- <FilterButton filterName="All"/>
         <FilterButton filterName="Playlists"/>
         <FilterButton filterName="Artists"/>
         <FilterButton filterName="Profile"/>
         <FilterButton filterName="Songs"/>
-        <FilterButton filterName="Albums"/>
+        <FilterButton filterName="Albums"/> -->
+        <FilterButton 
+        :class="isSelected ? 'selectedFilter':''"
+        :filterName="filter" 
+        :id="filter" 
+        @filterEvent="filterHandler"/>
     </div>
 </template>
  
 <style scoped>
-.radioFilter:checked + label{
+.selectedFilter{
     background-color: #C493E1;
 }
 </style>
