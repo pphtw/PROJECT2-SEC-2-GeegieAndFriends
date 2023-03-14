3e7fc61a5288be8435544c5ab6d43d86631392c2
Bowonwit Anothaisintavee <Bowonwit.A@gmail.com>
2023-03-14 23:52:09 +0700

    Changes by Siripoom

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,3 +9,14 @@ export const attrsBinder = (n, ...state) => {
       return state[n]
   }
 }
+export const shuffleArray = (array) => {
+  const firstItem = array[0]
+  const restOfQueue = array.filter((e, i) => i !== 0)
+  for (let i = restOfQueue.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = restOfQueue[i]
+    restOfQueue[i] = restOfQueue[j]
+    restOfQueue[j] = temp
+  }
+  return [firstItem, ...restOfQueue]
+}
