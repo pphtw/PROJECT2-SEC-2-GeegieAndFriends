28a0618688d7e38e34fc2a01e54315f64f51961f
pphtw <phutawan.pram@gmail.com>
2023-03-20 11:06:08 +0700

    [ADD] Search Engine

--- src/components/UI/molecules/SearchBar.vue
+++ src/components/UI/molecules/SearchBar.vue
@@ -9,7 +9,7 @@ const searchKeyword = ref('')
   <div class="h-10 flex rounded-full bg-[#E5E5E5] gap-3 px-3 py-1.5">
     <SearchIcon stroke="black" class="flex-none self-center" />
     <input
-      @change="$emit('searchEvent', searchKeyword)"
+      @input="$emit('searchEvent', searchKeyword)"
       v-model="searchKeyword"
       class="bg-transparent flex-1"
       type="text w-full"
