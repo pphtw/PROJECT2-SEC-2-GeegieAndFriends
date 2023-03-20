859b690af8e42ab99f1927a505fdddeb8b56ec53
pphtw <phutawan.pram@gmail.com>
2023-03-20 19:19:38 +0700

    [FIX] Search Engine

--- src/components/UI/organisms/TrackList.vue
+++ src/components/UI/organisms/TrackList.vue
@@ -3,18 +3,30 @@ import SingleTrack from './SingleTrack.vue'
 import { useControllerStore } from '@/stores/controllerStore'
 import { usePlaylistStore } from '@/stores/playlistStore'
 import { storeToRefs } from 'pinia'
+import { useSearchStore } from '@/stores/searchStore'
 
 // Use Store
 const playlistStore = usePlaylistStore()
 const controllerStore = useControllerStore()
+const searchStore = useSearchStore()
 
 const { selectedPlaylist } = storeToRefs(playlistStore)
 const { currentTrack } = storeToRefs(controllerStore)
+// const { filteredList } = storeToRefs(searchStore)
+
+// const props = defineProps({
+//   tracklist: {
+//     type: Object,
+//     require: true,
+//   },
+// })
+
+// console.log(props.tracklist)
 </script>
 
 <template>
   <div class="rounded-2xl no-scrollbar h-full scroll-smooth overflow-y-scroll">
-    <!-- #TrendingList -->
+    <!-- #TrackList -->
     <div
       class="flex items-center mb-1 h-20 bg-[#E5E5E5] hover:bg-[#D4D4D4] transition ease-in-out rounded-2xl overflow-clip cursor-pointer"
       v-for="(track, index) in selectedPlaylist"
