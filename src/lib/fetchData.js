1df6f4a408902315e550930a0f2fe2203ec6c2c0
Banlearit <banlearit.siri@gmail.com>
2023-03-22 05:56:19 +0700

    fix fetchData.js

--- src/lib/fetchData.js
+++ src/lib/fetchData.js
@@ -1,8 +1,10 @@
+const API_URL = 'http://localhost:5000'
 export const fetchTracksData = async () => {
   try {
-    const response = await fetch('http://localhost:5000/tracks')
+    const response = await fetch(`${API_URL}/tracks`)
     if (response.ok) {
-      return await response.json()
+      const tracks = await response.json();
+      return tracks;
     } else throw new Error('Network response was not ok')
   } catch (error) {
     console.error('Error fetching tracks data:', error.message)
@@ -10,9 +12,10 @@ export const fetchTracksData = async () => {
 }
 export const fetchPlaylistsData = async () => {
   try {
-    const response = await fetch('http://localhost:5000/playlists')
+    const response = await fetch(`${API_URL}/playlists`)
     if (response.ok) {
-      return await response.json()
+      const playlists = await response.json();
+      return playlists;
     } else throw new Error('Network response was not ok')
   } catch (error) {
     console.error('Error fetching playlists data:', error.message)
--- src/lib/getData.js
+++ src/lib/getData.js
@@ -1,32 +1,50 @@
 import { fetchPlaylistsData, fetchTracksData } from '@/lib/fetchData'
 
-const [trackData, playlistData] = await Promise.all([
-  fetchTracksData(),
-  fetchPlaylistsData(),
-])
+let trackData;
+let playlistData;
+export const loadData = () => {
+    return Promise.all([fetchTracksData(), fetchPlaylistsData()])
+        .then(([tracks, playlists]) => {
+            trackData = Object.values(tracks);
+            playlistData = Object.values(playlists);
+        })
+        .catch((error) => {
+            console.error('Error fetching data:', error.message);
+        });
+};
+
+loadData().then(() => {
+    console.log('Data loaded successfully');
+}).catch((error) => {
+    console.error('Error loading data:', error.message);
+});
 
 export const getAllTracks = () => {
-  return trackData
+    return trackData;
 }
 
 export const getTrack = (trackId = 1) => {
-  return trackData.find((track) => track['trackId'] === trackId)
+    return trackData.find((track) => track['trackId'] === trackId);
 }
+
 export const getTrackIdList = (playlistId) => {
-  return [
-    ...playlistData.find(
-      (playlist) => playlist['playlistId'] === Number(playlistId)
-    ).tracks,
-  ]
+    return [
+        ...playlistData.find(
+            (playlist) => playlist['playlistId'] === Number(playlistId)
+        ).tracks,
+    ];
 }
+
 export const getAllPlaylists = () => {
-  return playlistData
+    return playlistData;
 }
+
 export const getPlaylist = (playlistId) => {
-  return playlistData.find(
-    (playlist) => playlist['playlistId'] === Number(playlistId)
-  )
+    return playlistData.find(
+        (playlist) => playlist['playlistId'] === Number(playlistId)
+    );
 }
+
 export const getTrackList = (playlistId) => {
-  return getPlaylist(playlistId).tracks.map((trackId) => getTrack(trackId))
+    return getPlaylist(playlistId).tracks.map((trackId) => getTrack(trackId));
 }
