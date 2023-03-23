122023b422598a9a0c25720424be86893cfeb774
pphtw <phutawan.palakavong@gmail.kmutt.ac.th>
2023-03-23 21:56:02 +0700

    [FIXED] conflict

--- src/components/pages/SearchPage.vue
+++ src/components/pages/SearchPage.vue
@@ -11,18 +11,18 @@ import ContentSection from '../templates/ContentSection.vue'
 import { storeToRefs } from 'pinia'
 import { useSearchStore } from '@/stores/searchStore'
 import TrackList from '../UI/organisms/TrackList.vue'
-import { useControllerStore } from '@/stores/controllerStore'
+import PageTemplate from '@/components/templates/PageTemplate.vue'
 
 const searchStore = useSearchStore()
-const controllerStore = useControllerStore()
-
 const { filteredList, regex } = storeToRefs(searchStore)
-const { chooseTrack } = controllerStore
 
-// Definition
-const emit = defineEmits(['chooseTrack'])
+const props = defineProps({
+  isProgressBarClicked: {
+    type: Boolean,
+    required: true,
+  },
+})
 
-// handler
 const searchHandler = (input) => {
   regex.value = new RegExp(`${input}`, 'ig')
   console.log(regex.value)
@@ -36,48 +36,33 @@ const searchHandler = (input) => {
       .map((track) => track.trackId)
   )
 }
-
-const onChooseTrackClick = (e) => {
-  chooseTrack(e.currentTarget.id)
-  emit('chooseTrack', 300)
-}
 </script>
 
 <template>
-  <div class="container-gradient w-screen h-screen flex flex-row bg-[#3f5586]">
-    <NavigationBar />
-    <div
-      class="w-full h-full px-[4vw] py-8 gap-y-8 gap-x-[4vw] grid grid-cols-4"
-    >
-      <div class="flex flex-col col-span-3 min-h-full">
-        <!-- #SearchBarSection -->
-        <SearchBar class="h-full" @searchEvent="searchHandler" />
+  <PageTemplate
+    :is-progress-bar-clicked="isProgressBarClicked"
+    content-style="grid-cols-[3fr_minmax(18rem,1fr)] grid-rows-[2fr_5fr]"
+  >
+    <ContentSection class="flex flex-col row-span-2 min-h-full">
+      <!-- #SearchBarSection -->
+      <SearchBar class="h-full" @searchEvent="searchHandler" />
 
-        <!-- #FilterSection -->
-        <FilterSection class="h-fit" />
+      <!-- #FilterSection -->
+      <FilterSection class="h-fit" />
 
-        <!-- #TrackSection -->
-        <ContentSection class="min-h-0">
-          <TrackList
-            :tracklist="filteredList"
-            @choose-track="(e) => onChooseTrackClick(e)"
-          />
-        </ContentSection>
-      </div>
-
-      <div class="h-full flex flex-col gap-y-3">
-        <!-- #PlaylistSection -->
-        <div class="h-2/5">
-          <PlaylistCarousel />
-        </div>
-
-        <!-- #MusicPlayerCardSection -->
-        <div class="h-3/5">
-          <MusicPlayerCard />
-        </div>
-      </div>
-    </div>
-  </div>
+      <!-- #TrackSection -->
+      <TrackList
+        :trackList="filteredList"
+        @on-choose-track-click="(e) => onChooseTrackClick(e)"
+      />
+    </ContentSection>
+    <ContentSection>
+      <PlaylistCarousel />
+    </ContentSection>
+    <ContentSection>
+      <MusicPlayerCard />
+    </ContentSection>
+  </PageTemplate>
 </template>
 
 <style scoped>
