b37637a5d7311e78da1d5a88f85e59b11992222a
PhuMiZz <siripoom.kuso@kmutt.ac.th>
2023-03-29 13:38:24 +0700

    [Fix] layout in playlist overlay

--- src/components/UI/organisms/PlaylistOverlay.vue
+++ src/components/UI/organisms/PlaylistOverlay.vue
@@ -11,51 +11,53 @@ const props = defineProps({
 </script>
 
 <template>
-  <Transition>
-    <div
-      v-if="isClicked"
-      class="absolute top-0 left-0 w-screen h-screen bg-gray-900/50 flex items-center justify-center px-5 py-5 z-[999]"
-      @click.self="$emit('closeOverlay')"
-    >
+  <Teleport to="body">
+    <Transition>
       <div
-        class="grid grid-rows-[1fr_2fr] background-overlay rounded-3xl shadow-xl w-full h-full overflow-hidden"
-        style="max-width: 1000px"
+        v-if="isClicked"
+        class="absolute top-0 left-0 w-screen h-screen bg-gray-900/50 flex items-center justify-center z-[999]"
+        @click.self="$emit('closeOverlay')"
       >
-        <div class="md:flex w-full row-span-1 p-10">
-          <ContentSection class="min-h-full">
-            <template v-slot:header>
-              <div class="flex flex-row justify-between">
-                <PreviousPageButton @click="$emit('closeOverlay')" /></div
-            ></template>
+        <div
+          class="grid grid-rows-[1fr_2fr] background-overlay shadow-xl w-full h-full overflow-hidden"
+          style="max-width: 1000px"
+        >
+          <div class="md:flex w-full row-span-1 p-10">
+            <ContentSection class="min-h-full">
+              <template v-slot:header>
+                <div class="flex flex-row justify-between">
+                  <PreviousPageButton @click="$emit('closeOverlay')" /></div
+              ></template>
 
-            <div class="w-full h-full grid grid-cols-[1fr_2fr] gap-x-5">
-              <div class="bg-white aspect-square w-full"></div>
-              <div class="flex h-full items-center">
-                <div class="grow text-left text-white pt-20">
-                  <h1 class="text-6xl pb-5">Playlist Name #1</h1>
-                  <p class="text-2xl">(Owner) | (n) Song</p>
+              <div class="w-full h-full grid grid-cols-[1fr_2fr] gap-x-5">
+                <div class="bg-white aspect-square w-full"></div>
+                <div class="flex h-full items-center">
+                  <div class="grow text-left text-white pt-20">
+                    <h1 class="text-6xl pb-5">Playlist Name #1</h1>
+                    <p class="text-2xl">(Owner) | (n) Song</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          </ContentSection>
-        </div>
-        <div class="md:flex w-full row-span-1 p-10">
-          <ContentSection class="min-h-full">
-            <template v-slot:header>
-              <div
-                class="flex flex-row justify-start w-full gap-x-5 items-center"
-              >
-                <PlayPauseButton class="w-20 h-20" /><LikeButton
-                  class="w-10 h-10"
-                /><MenuButton class="w-10 h-10" /></div
-            ></template>
+            </ContentSection>
+          </div>
+          <div class="md:flex w-full row-span-1 p-10">
+            <ContentSection class="min-h-full">
+              <template v-slot:header>
+                <div
+                  class="flex flex-row justify-start w-full gap-x-5 items-center"
+                >
+                  <PlayPauseButton class="w-20 h-20" /><LikeButton
+                    class="w-10 h-10"
+                  /><MenuButton class="w-10 h-10" /></div
+              ></template>
 
-            <div class="w-full h-full grid grid-cols-[1fr_2fr] gap-x-5"></div>
-          </ContentSection>
+              <div class="w-full h-full grid grid-cols-[1fr_2fr] gap-x-5"></div>
+            </ContentSection>
+          </div>
         </div>
       </div>
-    </div>
-  </Transition>
+    </Transition>
+  </Teleport>
 </template>
 
 <style scoped>
