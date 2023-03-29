8ab9829a0fd83d1e196826e3d63d27d293613302
Banlearit <banlearit.siri@gmail.com>
2023-03-29 23:26:50 +0700

    Add: hashPassword

--- src/lib/util.js
+++ src/lib/util.js
@@ -21,3 +21,10 @@ export const shuffleArray = (array) => {
   }
   return [firstItem, ...restOfQueue]
 }
+export const hashPassword = async (password) => {
+  const encoder = new TextEncoder()
+  const data = encoder.encode(password)
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data)
+  const hashArray = Array.from(new Uint8Array(hashBuffer))
+  return hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
+}
