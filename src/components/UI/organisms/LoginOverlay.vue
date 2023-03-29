942b46b636f653eb3ea542d3cd9ca4371228da6c
pphtw <phutawan.palakavong@gmail.kmutt.ac.th>
2023-03-29 20:45:27 +0700

    [FIX]Login Function

--- src/components/UI/organisms/LoginOverlay.vue
+++ src/components/UI/organisms/LoginOverlay.vue
@@ -9,10 +9,10 @@ const overlayStore = useOverlayStore()
 
 const { openLoginOverlay } = storeToRefs(overlayStore)
 const { toggleLoginOverlay } = overlayStore
-const {message} = storeToRefs(userStore)
 
 const show = ref('login')
 const register = async () => {
+  // console.log(Object.values(userStore.user))
   await userStore.register(userStore.user)
 }
 </script>
@@ -165,112 +165,120 @@ const register = async () => {
                 <p>Enter your information to register</p>
               </div>
               <div>
-                <form @submit.prevent="register">
-                  <div class="flex -mx-3">
-                    <div class="w-1/2 px-3 mb-5">
-                      <label for="" class="text-xs font-semibold px-1"
-                        >First name</label
-                      >
-                      <div class="flex">
-                        <div
-                          class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
-                        >
-                          <i
-                            class="mdi mdi-account-outline text-gray-400 text-lg"
-                          ></i>
-                        </div>
-                        <input
-                          v-model="userStore.user.firstName"
-                          type="text"
-                          class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
-                          placeholder="John"
-                        />
-                      </div>
-                    </div>
-                    <div class="w-1/2 px-3 mb-5">
-                      <label for="" class="text-xs font-semibold px-1"
-                        >Last name</label
-                      >
-                      <div class="flex">
-                        <div
-                          class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
-                        >
-                          <i
-                            class="mdi mdi-account-outline text-gray-400 text-lg"
-                          ></i>
-                        </div>
-                        <input
-                          v-model="userStore.user.listName"
-                          type="text"
-                          class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
-                          placeholder="Smith"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div class="flex -mx-3">
-                    <div class="w-full px-3 mb-5">
-                      <label for="" class="text-xs font-semibold px-1"
-                        >Email</label
-                      >
-                      <div class="flex">
-                        <div
-                          class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
-                        >
-                          <i
-                            class="mdi mdi-email-outline text-gray-400 text-lg"
-                          ></i>
-                        </div>
-                        <input
-                          v-model="userStore.user.email"
-                          type="email"
-                          class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
-                          placeholder="johnsmith@example.com"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div class="flex -mx-3">
-                    <div class="w-full px-3 mb-12">
-                      <label for="" class="text-xs font-semibold px-1"
-                        >Password</label
-                      >
-                      <div class="flex">
-                        <div
-                          class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
-                        >
-                          <i
-                            class="mdi mdi-lock-outline text-gray-400 text-lg"
-                          ></i>
-                        </div>
-                        <input
-                          v-model="userStore.user.password"
-                          type="password"
-                          class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
-                          placeholder="************"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div class="flex -mx-3">
-                    <div class="w-full px-3 flex flex-col gap-4">
-                      <button type="submit"
-                        class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold"
-                      >
-                        REGISTER NOW
-                      </button>
+                <div class="flex -mx-3">
+                  <div class="w-1/2 px-3 mb-5">
+                    <label for="" class="text-xs font-semibold px-1"
+                      >First name</label
+                    >
+                    <div class="flex">
                       <div
-                        v-if="userStore.message"
-                        class="text-center mt-4 text-green-500"
+                        class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
                       >
-                        {{ userStore.message }}
+                        <i
+                          class="mdi mdi-account-outline text-gray-400 text-lg"
+                        ></i>
                       </div>
-                      <button @click="show = 'login'" class="hover:font-medium">
-                        Already a member?
-                      </button>
+                      <input
+                        v-model="userStore.user.firstName"
+                        type="text"
+                        class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                        placeholder="John"
+                        required
+                      />
                     </div>
                   </div>
-                </form>
+                  <div class="w-1/2 px-3 mb-5">
+                    <label for="" class="text-xs font-semibold px-1"
+                      >Last name</label
+                    >
+                    <div class="flex">
+                      <div
+                        class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
+                      >
+                        <i
+                          class="mdi mdi-account-outline text-gray-400 text-lg"
+                        ></i>
+                      </div>
+                      <input
+                        v-model="userStore.user.lastName"
+                        type="text"
+                        class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                        placeholder="Smith"
+                        required
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div class="flex -mx-3">
+                  <div class="w-full px-3 mb-5">
+                    <label for="" class="text-xs font-semibold px-1"
+                      >Email</label
+                    >
+                    <div class="flex">
+                      <div
+                        class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
+                      >
+                        <i
+                          class="mdi mdi-email-outline text-gray-400 text-lg"
+                        ></i>
+                      </div>
+                      <input
+                        v-model="userStore.user.email"
+                        type="email"
+                        class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                        placeholder="johnsmith@example.com"
+                        required
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div class="flex -mx-3">
+                  <div class="w-full px-3 mb-12">
+                    <label for="" class="text-xs font-semibold px-1"
+                      >Password</label
+                    >
+                    <div class="flex">
+                      <div
+                        class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
+                      >
+                        <i
+                          class="mdi mdi-lock-outline text-gray-400 text-lg"
+                        ></i>
+                      </div>
+                      <input
+                        v-model="userStore.user.password"
+                        type="password"
+                        class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                        placeholder="************"
+                        required
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div class="flex -mx-3">
+                  <div class="w-full px-3 flex flex-col gap-4">
+                    <button
+                      @click="register"
+                      class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold"
+                    >
+                      REGISTER NOW
+                    </button>
+                    <div
+                      v-if="userStore.message"
+                      class="text-center mt-4"
+                      :class="
+                        userStore.isRegistered
+                          ? 'text-green-500'
+                          : 'text-red-500'
+                      "
+                    >
+                      {{ userStore.message }}
+                    </div>
+                    <button @click="show = 'login'" class="hover:font-medium">
+                      Already a member?
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
             <div
--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -5,25 +5,31 @@ import { ref } from 'vue'
 export const useUserStore = defineStore('user', () => {
   const user = {
     firstName: '',
-    listName: '',
+    lastName: '',
     email: '',
     password: '',
   }
   const message = ref('')
+  const isRegistered = ref(false)
 
   const register = async (user) => {
     const userService = new UserService()
     try {
-      const registeredUser = await userService.registerUser(user)
-      if (registeredUser) {
-        console.log('User registered:', registeredUser)
-        message.value = 'Registration successful!'
-        return true
+      if (Object.values(user).filter((e) => e).length === 0) {
+        message.value = 'Please insert your information!'
+        isRegistered.value = false
+      } else {
+        const registeredUser = await userService.registerUser(user)
+        if (registeredUser) {
+          console.log('User registered:', registeredUser)
+          message.value = 'Registration successful!'
+          isRegistered.value = true
+        }
       }
     } catch (e) {
       console.error(`Error registering user: ${e.message}`)
-      return false
+      isRegistered.value = false
     }
   }
-  return { user, message, register }
+  return { user, message, register, isRegistered }
 })
