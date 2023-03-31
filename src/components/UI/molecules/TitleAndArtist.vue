c45fe59b9826f3f95bef6d47d9aae17d4206cb68
Bowonwit Anothaisintavee <Bowonwit.A@gmail.com>
2023-04-01 00:28:40 +0700

    add drag and drop track list

--- src/components/UI/molecules/TitleAndArtist.vue
+++ src/components/UI/molecules/TitleAndArtist.vue
@@ -8,7 +8,10 @@ const props = defineProps({
 const { track } = props
 </script>
 <template>
-  <div class="grow grid grid-rows-2 h-fit max-sm:w-full pl-3 sm:pl-5">
+  <div
+    class="grow grid grid-rows-2 h-fit max-sm:w-full pl-3 sm:pl-5"
+    draggable="false"
+  >
     <MusicName :font-weight="3" text-size="xl">{{ track.name }}</MusicName>
     <MusicName :font-weight="2"> {{ track.artist }}</MusicName>
   </div>
