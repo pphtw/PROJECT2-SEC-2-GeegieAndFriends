7ebf96eed924031742274ad5466c7c43b670eca2
PhuMiZz <siripoom.kuso@kmutt.ac.th>
2023-03-31 21:59:51 +0700

    [FIX] PlaylistStore

--- src/stores/searchStore.js
+++ src/stores/searchStore.js
@@ -32,14 +32,8 @@ export const useSearchStore = defineStore('search', () => {
   const checkKeywords = (keyword) => keyword.match(regex.value)
 
   onMounted(async () => {
-    if (Object.keys(currentUser.value).length !== 0) {
-      filteredPlaylists.value = await userService.getUserPlaylists(
-        currentUser.value.id
-      )
-    } else {
-      filteredPlaylists.value = await userService.getUserPlaylists(1)
-    }
     filteredTrackList.value = await trackService.getAllItems('tracks')
+    filteredPlaylists.value = await trackService.getAllItems('playlists')
   })
   // filteredTrackList.value = await trackService.getAllItems('tracks')
   // filteredPlaylists.value = await trackService.getAllItems('playlists')
