47fe182da301fd3cfb578f4907dfd27fd91e62c4
pphtw <phutawan.palakavong@gmail.kmutt.ac.th>
2023-03-31 16:16:14 +0700

    [FIXING] Register

---
+++ src/lib/registerManagement.js
@@ -0,0 +1,21 @@
+const registerManagement = () => {
+  const userPattern = {
+    firstName: /^[A-Za-z]+$/,
+    lastName: /^[A-Za-z]+$/,
+    email: /.+@([a-zA-Z0-9\-]+)\.com/,
+    password: /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{7,})/,
+  }
+  const checkPattern = (user, type) => {
+    return type === 'firstName'
+      ? userPattern.firstName.test(user.firstName)
+      : type === 'lastName'
+      ? userPattern.lastName.test(user.lastName)
+      : type === 'email'
+      ? userPattern.email.test(user.email)
+      : type === 'password'
+      ? userPattern.password.test(user.password)
+      : false
+  }
+  return { checkPattern }
+}
+export { registerManagement }
