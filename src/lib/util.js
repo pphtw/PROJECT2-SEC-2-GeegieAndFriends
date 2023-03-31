ad33cec52fb4024c6b416ee4c9efce0292293b97
Banlearit <banlearit.siri@gmail.com>
2023-03-31 08:49:22 +0700

    Fix login

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,3 +1,4 @@
+
 export const secToMin = (timeInSec= 0) => {
 
   return new Date(isNaN(timeInSec)?0:timeInSec * 1000).toISOString().substring(14, 19)
@@ -26,6 +27,12 @@ export const hashPassword = async (password) => {
   const data = encoder.encode(password)
   const hashBuffer = await crypto.subtle.digest('SHA-256', data)
   const hashArray = Array.from(new Uint8Array(hashBuffer))
+  console.log(hashBuffer)
+  console.log(`encoder : ${encoder}`)
+  console.log(`data : ${data}`)
+  console.log(`hashBuffer: ${hashBuffer}`)
+  console.log(`hashArray : ${hashArray}`)
+  console.log('return hash :' + hashArray.map(b => b.toString(16).padStart(2, '0')).join(''))
   return hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
 }
 export function savePlaybackState(queue, dumpQueue, currentPlaylistId) {
