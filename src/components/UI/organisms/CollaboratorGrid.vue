7803a241d525b143d390a4758f5e9d09e6761d7b
PhuMiZz <siripoom.kuso@kmutt.ac.th>
2023-04-02 03:23:29 +0700

    [ADD] add picture & fix router

--- src/components/UI/organisms/CollaboratorGrid.vue
+++ src/components/UI/organisms/CollaboratorGrid.vue
@@ -10,9 +10,11 @@
     ></div>
     <div
       class="bg-amber-300 bg-cover bg-no-repeat bg-center h-[90vh] xl:h-full hover:bg-amber-300/70 hover:opacity-70"
+      style="background-image: url(/img/Banlearit.jpg)"
     ></div>
     <div
       class="bg-emerald-300 bg-cover bg-no-repeat bg-center h-[90vh] xl:h-full hover:bg-emerald-300/70 hover:opacity-70"
+      style="background-image: url(/img/Siripoom.jpg)"
     ></div>
     <div
       class="bg-fuchsia-300 bg-cover bg-no-repeat bg-center h-[90vh] xl:h-full hover:bg-fuchsia-300/70 hover:opacity-70"
